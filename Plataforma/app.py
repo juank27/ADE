c6aa8726e1cceff636d42e396f4adf1d8feb1233
juank27 <juankruiz27@gmail.com>
2021-04-21 21:01:28 -0500

    prueba fallida con hilo del selector

--- Plataforma/app.py
+++ Plataforma/app.py
@@ -1,6 +1,8 @@
 import flask
 import threading
-from flask import render_template,request
+from flask import g #variable global
+from flask import render_template,request, copy_current_request_context, flash
+from flask_mysqldb import MySQL
 from flask_mail import Mail,Message
 from flask_mysqldb import MySQL
 from principal import inicio
@@ -24,6 +26,17 @@ app.config['MAIL_USE_SSL'] = True
 
 mail = Mail()
 
+def send_email(user_email, asunto):
+	msg = Message(asunto, sender = 'adeplataforma@gmail.com', recipients = [user_email] )
+	msg.html = render_template('principal/Registro_institucion.html')
+	mail.send(msg)
+
+def cusulta_institucion():
+	cur = mysql.connection.cursor()
+	cur.execute('SELECT NOMBRE_ESTABLECIMIENTO FROM institucion_1')
+	data = cur.fetchall()
+	return data
+	
 @app.route('/')
 def index():
 	return render_template('principal/index.html')
@@ -32,10 +45,19 @@ app.register_blueprint(inicio)
 
 @app.route('/contacto')
 def contacto():
-	cur = mysql.connection.cursor()
-	cur.execute('SELECT NOMBRE_ESTABLECIMIENTO FROM institucion_1')
-	data = cur.fetchall()
-	return render_template('principal/contacto.html', institucion = data)
+	#cur = mysql.connection.cursor()
+	#cur.execute('SELECT NOMBRE_ESTABLECIMIENTO FROM institucion_1')
+	#data = cur.fetchall()
+	@copy_current_request_context
+	def send_consulta():
+		ola = cusulta_institucion()
+		return ola
+	sender = threading.Thread(name = 'cunsulta', target = send_consulta, args = ())
+	sender.start()
+	print(send_consulta())
+	#flash(send_consulta())
+	#ss = send_consulta()
+	return render_template('principal/contacto.html', institucion = send_consulta())
 
 #configuracion 
 @app.route('/enviarInscripciones' , methods=['GET','POST'])
@@ -44,16 +66,22 @@ def enviarInscripciones():
 		nameRector = request.form['nameRector']
 		email = request.form['email']
 		telefono = request.form['telefono']
-		nameInstitucion = request.form['nameInstitucion']
+		#nameInstitucion = request.form['nameInstitucion']
 		codDane = request.form['codDane']
 		municipio = request.form['municipio']
 		numUsuarios = request.form['numUsuarios']
 		comentarios = request.form['comentarios']
-		asunto = 'Registro ' + nameInstitucion
-		msg = Message(asunto, sender = 'adeplataforma@gmail.com', recipients = [email] )
-		msg.html = render_template('principal/Registro_institucion.html' , nombre = nameRector)
-		mail.send(msg)
- 
+		asunto = 'Registro ' 
+		#msg = Message(asunto, sender = 'adeplataforma@gmail.com', recipients = [email] )
+		#msg.html = render_template('principal/Registro_institucion.html' , nombre = nameRector)
+		#mail.send(msg)
+		#print(email)
+		#Envio del correo 
+		@copy_current_request_context 
+		def send_message(email, asunto):
+			send_email(email, asunto)
+		sender = threading.Thread(name = 'mailll', target = send_message, args = (email , asunto))
+		sender.start()
 	return render_template('principal/index.html')
 
 if __name__ == '__main__':
